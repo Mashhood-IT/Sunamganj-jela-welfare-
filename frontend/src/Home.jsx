a364117239e68f8ffc3a5c5f37e9896ecf4d5964
Mashhood-IT <mltcontrol10@gmail.com>
2025-10-24 11:03:15 +0500

    removed donation flow

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Hero from './components/HomeComponents/Hero'
 import About from './components/HomeComponents/About'
 import FAQ from './components/HomeComponents/FAQ'
-import DonationFlow from './components/HomeComponents/DonationFlow'
 import Solution from './components/HomeComponents/Solution'
 
 const Home = () => {
@@ -11,7 +10,6 @@ const Home = () => {
     <Hero />
     <About />
     <Solution />
-    <DonationFlow />
     <FAQ />
     </div>
   )
