891a443fadbef0efa0188e8362dc00b65d8f3be3
Mashhood-IT <mltcontrol10@gmail.com>
2025-11-03 20:07:29 +0500

    done

--- frontend/src/components/ContactComponents/JoinUs.jsx
+++ frontend/src/components/ContactComponents/JoinUs.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 
 const JoinUs = () => (
-  <div className="w-full  py-16 px-4">
-    <div className="max-w-4xl mx-auto text-center">
-      <h2 className="text-4xl font-bold mb-6 text-gray-900">Join Us</h2>
-      <p className="text-gray-700 leading-relaxed text-lg mb-4">
-        We invite you to reach out, not just to ask but to connect. Share your thoughts, your experiences, or your ideas. Be part of a team that believes in compassion and unity.
-      </p>
-      <p className="text-gray-700 leading-relaxed text-lg mb-4">
-        At Sunamganj Jela Welfare Association UK, every message begins a new friendship and every friendship builds a better world.
-      </p>
-      <p className="text-gray-700 leading-relaxed text-lg mb-4">
-        We are always ready to listen, ready to help, and ready to welcome you.
-      </p>
-      <p className="text-gray-900 leading-relaxed text-xl font-bold ">
-        Contact us today. Together, let's keep kindness alive.
-      </p>
+  <div className="w-full bg-(--main-green-color) text-white py-16 px-6">
+    <div className="max-w-3xl mx-auto text-center">
+      <h2 className="lg:text-4xl text-2xl font-bold mb-6 text-(--main-orange-color)">
+        Join Us
+      </h2>
+      <div className="space-y-5 leading-relaxed lg:text-lg text-md">
+        <p>
+          We invite you to reach out, not just to ask but to connect. 
+          Share your thoughts, your experiences, or your ideas. 
+          Be part of a team that believes in compassion and unity.
+        </p>
+        <p>
+          At Sunamganj Jela Welfare Association UK, every message begins a new friendship 
+          and every friendship builds a better world.
+        </p>
+        <p>
+          We are always ready to listen, ready to help, and ready to welcome you.
+        </p>
+        <p className="text-xl font-bold text-(--main-orange-color) mt-4">
+          Contact us today. Together, let's keep kindness alive.
+        </p>
+      </div>
     </div>
   </div>
 );
+
 export default JoinUs;
