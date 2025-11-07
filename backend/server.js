b5e17534d5f98c8eff32e65a3c350cf3e50bda70
Mashhood-IT <mltcontrol10@gmail.com>
2025-11-07 23:06:36 +0500

    backend done

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,11 @@ const app = express()
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}))
 connectDB;
 app.use("/api",memberShipRoutes)
 app.use("/api/stripe", stripeRoutes)
