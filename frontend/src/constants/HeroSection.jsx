b31b23a0245e13b184f3f488b8f8c0f532e39308
Mashhood-IT <mltcontrol10@gmail.com>
2025-11-13 15:05:28 +0500

    mobile view changes

--- frontend/src/constants/HeroSection.jsx
+++ frontend/src/constants/HeroSection.jsx
@@ -5,7 +5,7 @@ const HeroSection = ({ title, description }) => {
     <section className="bg-linear-to-br from-[#00A162] via-[#008a54] to-[#1a8d5f] py-32 px-4">
       <div className="max-w-7xl mx-auto text-center mt-4 space-y-4">
         <h1 className="md:text-4xl text-2xl font-bold text-white pt-[55px] ">{title}</h1>
-        <p className="text-xl text-gray-100 max-w-2xl mx-auto">{description}</p>
+        <p className="md:text-xl text-lg text-gray-100 max-w-2xl mx-auto">{description}</p>
       </div>
     </section>
   );
